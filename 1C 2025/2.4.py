2ad7ab3017fe1decfd7242aac32eba36b263c6c9
Mariana Juarez Goldemberg <mjuarezg@fi.uba.ar>
2025-03-23 15:53:04 -0300

    Añado material 1C 2025

---
+++ 1C 2025/2.4.py
@@ -0,0 +1,16 @@
+# Guía 2 - Ej. 4
+# Crear un programa que le solicite al usuario:
+
+# Su nombre y lo imprima por pantalla.
+# Su edad y la imprima por pantalla.
+# Su edad, le sume 1, y la imprima por pantalla.
+
+nombre = input("Ingresá tu nombre: ")
+print(nombre)
+
+edad = input("Ingresá tu edad: ")
+print(edad)
+
+edad = int(input("Ingresá tu edad: "))
+edad_aumentada = edad + 1
+print(edad_aumentada)
---
+++ 1C 2025/2.5.py
@@ -0,0 +1,6 @@
+# Guía 2 - Ej. 5
+# Crear un programa que le solicite al usuario un número, y que devuelva el resto obtenido de dividirlo por 2.
+# ¿Qué operador vimos para obtener el resto?
+
+numero = int(input("Ingresá un número: "))
+print(numero % 2)
